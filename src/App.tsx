4ebda925995c58d7b30a4b93a152bd60a1e0bfdd
Israel Tavares <israelbulcaotavares@gmail.com>
2023-11-17 18:57:51 -0300

    styled component

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/Button"
+
+function App() {
+  
+  return (
+    <> 
+     
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="danger"/>
+      <Button variant="success"/>
+     
+    </>
+  )
+}
+
+export default App
