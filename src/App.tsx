101780b306dcc7651bffa1727fbc0451b76e5d4e
Israel Tavares <israelbulcaotavares@gmail.com>
2023-11-18 16:57:25 -0300

    configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Button } from "./components/Button"
-
+import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 function App() {
-  
   return (
-    <> 
-     
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="danger"/>
-      <Button variant="success"/>
-     
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="danger" />
+        <Button variant="success" />
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
