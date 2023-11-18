9deeaac515a340e6047915d624bbafeb0c612c81
Israel Tavares <israelbulcaotavares@gmail.com>
2023-11-18 17:13:48 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./components/Button";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 function App() {
   return (
     <>
@@ -9,6 +10,9 @@ function App() {
         <Button variant="secondary" />
         <Button variant="danger" />
         <Button variant="success" />
+        <Button />
+        
+        <GlobalStyle/>
       </ThemeProvider>
     </>
   );
