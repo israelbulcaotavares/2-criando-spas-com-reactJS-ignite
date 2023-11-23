ba157ab2667c103bd14a0f35ef94e3c86bff92da
Israel Tavares <israelbulcaotavares@gmail.com>
2023-11-23 20:22:11 -0300

    estilizando pagina history

--- src/pages/History.tsx
+++
@@ -1,7 +0,0 @@
-export function History() {
-  return (
-    <div>
-      <h1>História</h1>
-    </div>
-  )
-}
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -32,34 +32,33 @@ const BaseInput = styled.input`
   background: transparent;
   height: 2.5rem;
   border: 0;
-  border-bottom: 2px solid ${(props) => props.theme["gray-500"]} ;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
   font-weight: bold;
   font-size: 1.125rem;
-  padding: 0 0.5rem ;
-  color: ${(props) => props.theme["gray-100"]}; 
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme["gray-100"]};
 
-  &:focus{
+  &:focus {
     box-shadow: none;
     border-color: ${(props) => props.theme["green-500"]};
   }
 
   &::placeholder {
-    color: ${(props => props.theme["gray-500"])};
+    color: ${(props) => props.theme["gray-500"]};
   }
 `;
 
 export const TaskInput = styled(BaseInput)`
-    flex: 1; /* estica os elementos até aonde caber */
+  flex: 1; /* estica os elementos até aonde caber */
 
-    &::-webkit-calendar-picker-indicator { /* remove setinha p/ baixo no campo */
-        display: none !important ;
-
-    }
+  &::-webkit-calendar-picker-indicator {
+    /* remove setinha p/ baixo no campo */
+    display: none !important ;
+  }
 `;
 
-
 export const MinutesAmountInput = styled(BaseInput)`
-    width: 4rem;
+  width: 4rem;
 `;
 
 export const CountdownContainer = styled.div`
